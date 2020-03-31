f7ac7445a780018528a2a104f0c2fae1b2888e8c
450Oucema <450oucema@gmail.com>
2020-03-31 22:03:38 +0200

    Création de l'application Angular - Creation du projet, des components, des services et de l'authentification
    Arrêt au singlebookcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gestion-livres-ng';
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyCv9bx0FjrFVtR5n-8Fyu_ZqoEYVBkyYYk',
+      authDomain: 'angulartutoriel.firebaseapp.com',
+      databaseURL: 'https://angulartutoriel.firebaseio.com',
+      projectId: 'angulartutoriel',
+      storageBucket: 'angulartutoriel.appspot.com',
+      messagingSenderId: '727478537890',
+      appId: '1:727478537890:web:04aa1f7cf6ee950e9911fb'
+    };
+    firebase.initializeApp(config);
+  }
+
+
 }
---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Book} from '../models/book.model';
+import {Subscription} from 'rxjs';
+import {BookService} from '../services/book.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit, OnDestroy {
+
+  books: Book[];
+  booksSubscription: Subscription;
+
+  constructor(private bookService: BookService, private router: Router) { }
+
+  ngOnInit() {
+    this.booksSubscription = this.bookService.booksSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.bookService.emitBooks();
+  }
+
+  onNewBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+
+  onDeleteBook(book: Book) {
+    this.bookService.removeBook(book)
+  }
+
+  onViewBook(id: number) {
+    this.router.navigate(['/books','view', id]);
+  }
+
+  ngOnDestroy(): void {
+    this.booksSubscription.unsubscribe();
+  }
+}
